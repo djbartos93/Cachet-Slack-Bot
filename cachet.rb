d3fe996b89cdeef92258931b758cfbe57de57e8f
Eric Bartos <djbartos93@gmail.com>
2017-04-12 09:55:29 -0400

    somewhat less shit.

--- cachet.rb
+++ cachet.rb
@@ -2,11 +2,53 @@ require 'http'
 require 'json'
 require 'uri'
 require 'cachet'
+require 'influxdb'
 include Comparable
 
 
+#################====CACHET API STUFF===#########################
+
 #CachetClient = CachetClient.new(ENV['CACHET_API_KEY'], ENV['CACHET_BASE_URL'])
 CachetComponents = CachetComponents.new(ENV['CACHET_API_KEY'], ENV['CACHET_BASE_URL'])
+#CachetIncidents = CachetIncidents.new(ENV['CACHET_API_KEY'], ENV['CACHET_BASE_URL'])
+#CachetMetrics = CachetMetrics.new(ENV['CACHET_API_KEY'], ENV['CACHET_BASE_URL'])
+#CachetSubscribers = CachetSubscribers.new(ENV['CACHET_API_KEY'], ENV['CACHET_BASE_URL'])
+
+
+#################====write to InfluxDB stuff===#########################
+#this will write to influxdb
+def influx(component_id, component_name, status_id, status_name)
+  #lets define some stuff about our database, this should probablyl be env variables
+  #username = 'graf' #FUTURE
+  #password = 'graffing' #FUTURE
+  database = "home"
+  #name = valuename
+  host = "172.16.0.34"
+
+  influxdb = InfluxDB::Client.new  database: database, host: host
+  name = component_name
+  data = {
+    values: { component: component_id, status_id: status_id, },
+    tags:   { stat: component_name, readable: status_name } # tags are optional
+  }
+
+  influxdb.write_point(name, data)
+  puts data
+  #puts dataout
+end
+
+#wrtie to influxdb
+def cachet_read
+data = JSON.parse(CachetComponents.list)['data']
+puts data
+data.each do |d|
+  d['id'] = statusID
+end
+
+cachet_read
+
+
+
 #json1 = CachetComponents.list
 #json2 = CachetComponents.list
 #data = JSON.parse(components_pretty)['data']
@@ -16,34 +58,3 @@ CachetComponents = CachetComponents.new(ENV['CACHET_API_KEY'], ENV['CACHET_BASE_
 #  puts k
 #  sleep(3)
 #end
-#CachetIncidents = CachetIncidents.new(ENV['CACHET_API_KEY'], ENV['CACHET_BASE_URL'])
-#CachetMetrics = CachetMetrics.new(ENV['CACHET_API_KEY'], ENV['CACHET_BASE_URL'])
-#CachetSubscribers = CachetSubscribers.new(ENV['CACHET_API_KEY'], ENV['CACHET_BASE_URL'])
-
-
-def thing1
-  @read1 = CachetComponents.list['data']
-  sleep(4)
-  @read2 = CachetComponents.list['data']
-  @read1.each do |start1|
-    puts start1['id']
-    puts @read2['id']
-  end
-end
-
-def compare
-  @read1.each do |start1|
-    puts start1['id']
-    puts @read2['id']
-  end
-end
-
-def initialize
-  data_hash = JSON.parse(CachetComponents.list)
-  @id = data_hash['id']
-  @status = data_hash['status']
-  @name = data_hash['name']
-  @status_name = data_hash['status_name']
-end
-
-initialize
--- slack.rb
+++ slack.rb
@@ -3,8 +3,9 @@ require 'json'
 
 rc = HTTP.post("https://slack.com/api/chat.postMessage", params:{
   token: ENV['SLACK_API_TOKEN'],
-  channel: '#code',
-  text: 'Test service up message',
+  channel: 'test-emails',
+  text: 'no.',
+  attachments: 'really?',
   icon_emoji: ':white_check_mark:'
   })
 puts JSON.pretty_generate(JSON.parse(rc.body))
